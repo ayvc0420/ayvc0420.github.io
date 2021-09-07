1076af8908908a569df379e19675685ee8b4eecc
ayvc0420 <ayvc0420@gmail.com>
2021-09-07 15:43:27 +0800

    update
    降低照片檔案大小，提升使用者體驗
    scroll改變條件，動畫更早顯示
    recommendYoutube.html增加FB外掛留言板

--- js/aidoru.js
+++ js/aidoru.js
@@ -1,6 +1,6 @@
 
 (function($){$.fn.orzScroll=function(setting){var windowCenter=$(window).height()/2,orzProperty={animation:"fadeInDown",offset:0,delay:"0s"};var orzSetting=$.extend(orzProperty,setting);return this.each(function(){$(this).css("opacity","0").addClass("animated");var thisClass=$(this),thisClassTop=$(this).offset().top,thisClassBottom=thisClassTop+$(this).height()+orzProperty.offset,thisClassBottomOffset=thisClassBottom+windowCenter-130,thisAnimation=orzSetting.animation;$(window).scroll(function(){var currentScroll=
-    $(this).scrollTop(),currentScrollCenter=currentScroll+$(this).height()/2,currentScroll=$(this).height();if(currentScrollCenter>thisClassTop&&currentScrollCenter<thisClassBottomOffset){thisClass.addClass(thisAnimation);thisClass.css("opacity","1");
+    $(this).scrollTop(),currentScrollCenter=currentScroll+$(this).height()/2,currentScroll=$(this).height();if(currentScrollCenter+250>thisClassTop&&currentScrollCenter+250<thisClassBottomOffset){thisClass.addClass(thisAnimation);thisClass.css("opacity","1");
     //偵測離開該元素後將透明度改為0
     // thisClass.css("-webkit-animation-delay",orzProperty.delay);thisClass.css("animation-delay",orzProperty.delay)}else if(currentScroll>thisClassBottom||currentScrollBottom<thisClassTop){thisClass.removeClass(thisAnimation);thisClass.css("opacity",
     // "0")
