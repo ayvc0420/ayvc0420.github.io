7dc98394e9b40d919504b482ccff7219ac58befc
ayvc0420 <ayvc0420@gmail.com>
2021-09-10 19:05:26 +0800

    mob_btn
    animation update

--- js/work.js
+++ js/work.js
@@ -28,6 +28,7 @@ $(function(){
             const html_high = $(this).scrollTop();
             if(html_high >= 400){
                 $('#mob_top').css({'display':'block'})
+                $('#mob_top').addClass('mob_btn_anim');
             }else{
                 $('#mob_top').css({'display':'none'})
             }
