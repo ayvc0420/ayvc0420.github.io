c6214d99851e79a1e827db945b109cd7da945ec0
ayvc0420 <ayvc0420@gmail.com>
2021-10-12 19:59:56 +0800

    手機板模式上一頁svg顯示錯誤

--- js/layout.js
+++ js/layout.js
@@ -1,4 +1,5 @@
-// const mob_menu_div = document.getElementById('mob_menu_div')
+const mob_menu_div = document.getElementById('mob_menu_div')
+// const mob_menu = document.querySelector('.mob_menu')
 const mob_top = document.getElementById('mob_top')
 
 //svg切換
@@ -9,11 +10,14 @@ mob_menu_control.addEventListener('click',function(){
     const svg1_view = window.getComputedStyle(svg1,null).opacity
     // const svg2_view = window.getComputedStyle(svg2,null).opacity
     if(svg1_view === '1'){
-        svg1.style.opacity = '0';
-        svg2.style.opacity = '1';
-    }else{
-        svg1.style.opacity = '1';
-        svg2.style.opacity = '0';
+        svg1.style.opacity = 0;
+        svg2.style.opacity = 1;
+        mob_menu_div.style.transform = 'translateX(0%)'
+    }
+    if(svg1_view === '0'){
+        svg1.style.opacity = 1;
+        svg2.style.opacity = 0;
+        mob_menu_div.style.transform = 'translateX(-100%)'
     }
 })
 
