3a0a37669d413b7e4406b17f1817a597c6f70751
ayvc0420 <ayvc0420@gmail.com>
2021-11-23 21:33:04 +0800

    1123 Vtuber資料改為Vue footer資訊更改

--- js/introduce.js
+++ js/introduce.js
@@ -1,39 +1,42 @@
 
 window.addEventListener('load',function(){
 
-    const vt_player_all = document.querySelectorAll('.vt_player')
-    // const vt_player_alla = Array.from(document.querySelectorAll('.vt_player'))
+    // const vtPlayerAll = document.querySelectorAll('.vt_player')
+
+    // console.log(vtPlayerAll)
 
     window.addEventListener('scroll',function(){
-        //確保使用者拉縮畫面導致取頂距離不準確 不重整畫面顯示異常因此將取頂放入scroll監控內 
+        //確保使用者拉縮畫面導致取頂距離不準確 不重整畫面會顯示異常因此將取頂距離放入scroll監控內 
 
-        const now_height = document.documentElement.scrollTop;
-        const now_width = document.documentElement.scrollWidth;
+        const nowHeight = document.documentElement.scrollTop;
+        const nowWidth = document.documentElement.scrollWidth;
         const height = document.documentElement.clientHeight;
 
-        vt_player_all.forEach(item => {
-            const vtPlayerTop = item.offsetTop;
-            const vtControl = vtPlayerTop - height <= now_height-100;
-            if(vtControl === true){
-                item.classList.add('vt_player_ani')
-            }else{
-                item.classList.remove('vt_player_ani')
-            }
-            }
-        )
+        // console.log(vtPlayerAll)
+        //     vtPlayerAll.forEach(item =>{
+        //         const distanceTop = item.offsetTop;
+        //         console.log(distanceTop)
+        //         const Control = distanceTop - height <= nowHeight-100;
+        //         if(Control){
+        //             this.vtAni=true;
+        //         }else{
+        //             this.vtAni=false;
+        //         }
+        //     }
+        // )
 
 
-        if(now_width >= 1024){
+        if(nowWidth >= 1024){
             mob_top.style.display = 'none';
         }else{
             mob_top.style.display = 'block';
         }
-        if(height <= now_height){
+        if(height <= nowHeight){
             mob_top.style.transform = `translateX(0%)`
         }else{
             mob_top.style.transform = `translateX(1000%)`
         }
     })
-
+    
 
 })
---
+++ js/introduceVue.js
@@ -0,0 +1,87 @@
+
+    // const url = '../data/vtuber.json'
+    // let vtData = async function(){
+    //     let data;
+    //     try{
+    //         data = await axios(url)
+    //     }catch{
+    //         alert('資料獲取失敗，請稍後再試。')
+    //     }
+    //     return data
+    // }
+    // let data = vtData()
+    // console.log('v',data)
+
+window.addEventListener('load',async function(){
+    
+    let v = new Vue({
+        el:'#main',
+        data:{
+            vtuberData:[],
+            area:['全部','日本','台灣','印尼','EN'],
+            areaOption:'全部',
+            vtNumber:63,
+        },
+        methods: {
+            async fetchVtuber(){
+                this.vtuberData = [];
+                let data;
+                const url = '../data/vtuber.json'
+                try{
+                    data = await fetch(url)
+                }catch{
+                    alert('資料獲取失敗，請稍後再試。')
+                }
+                data.json().then((res)=>{
+                    res.vtuber.forEach(item=>{
+                        if(this.areaOption === this.area[0]){
+                            this.vtuberData.push(item)
+                        }
+                        if(item.area === this.areaOption){
+                            this.vtuberData.push(item)
+                        }
+                    })
+                })
+            },
+            vtAnimation(){
+                const vtPlayerAll = document.querySelectorAll('.vt_player')
+                //確保使用者拉縮畫面導致取頂距離不準確 不重整畫面會顯示異常因此將取頂放入scroll監控內 
+                const nowHeight = document.documentElement.scrollTop;
+                const height = document.documentElement.clientHeight;
+                vtPlayerAll.forEach(item =>{
+                    const distanceTop = item.offsetTop;
+                    const Control = distanceTop - height <= nowHeight-50;
+                    if(Control){
+                        item.classList.add('vt_player_ani');
+                    }else{
+                        item.classList.remove('vt_player_ani');
+                    }
+                })
+            },
+            vtN(){
+                // X 弄了我一小時才想到這方法
+                setTimeout(() => {                    
+                    const vtPlayerAll = document.querySelectorAll('.vt_player')
+                    let n=0;
+                    vtPlayerAll.forEach(item =>{
+                        if(window.getComputedStyle(item,null).display === 'flex'){
+                            n+=1;
+                        }
+                    })
+                    this.vtNumber = n;
+                }, 5);
+            },
+            
+        },
+        async mounted(){
+            await this.fetchVtuber()
+            await window.addEventListener('scroll',this.vtAnimation)
+            await document.getElementById('areaChange').addEventListener('change',this.vtN)
+            
+        }
+    })
+
+    // document.getElementById('areaChange').addEventListener('change',v.fetchVtuber)
+    // window.addEventListener('scroll',v.vtAnimation)
+    
+})
