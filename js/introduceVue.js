e5f3c1b7afb17db54d3aa6c823137afc8992b667
ayvc0420 <ayvc0420@gmail.com>
2021-12-10 03:01:16 +0800

    debounce

--- js/introduceVue.js
+++ js/introduceVue.js
@@ -73,11 +73,26 @@ window.addEventListener('load',async function(){
                     this.vtNumber = n;
                 }, 50);
             },
-            
+            // debounce(func, wait = 6, immediate = true) {
+            //     var timeout;
+            //     return function() {
+            //         var context = this, args = arguments;
+            //         var later = function() {
+            //             timeout = null;
+            //             if (!immediate) func.apply(context, args);
+            //         };
+            //         var callNow = immediate && !timeout;
+            //         clearTimeout(timeout);
+            //         timeout = setTimeout(later, wait);
+            //         if (callNow) func.apply(context, args);
+            //     };
+            // }
+
         },
         mounted(){
             this.fetchVtuber()
             window.addEventListener('scroll',this.vtAnimation)
+            // window.addEventListener('scroll',this.debounce(this.vtAnimation))
             document.getElementById('areaChange').addEventListener('change',this.vtN)
         }
     })
